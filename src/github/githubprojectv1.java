7bf0978a437e55ed8be45e4f1c6560a24dffb130
ssivanantham <ssivanantham@ITEM-S119985.emea.msad.sopra>
2022-08-30 12:09:49 +0530

    java project commit(first)

---
+++ src/github/githubprojectv1.java
@@ -0,0 +1,10 @@
+package github;
+
+public class githubprojectv1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Git hub demo");
+	}
+
+}
